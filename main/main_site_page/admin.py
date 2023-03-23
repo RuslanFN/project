a5d14c1279087425e13df2bc9cde0f836370e559
Your Name <you@example.com>
2023-03-23 15:06:00 +0500

    sdsvd

--- main/main_site_page/admin.py
+++ main/main_site_page/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import teacher, group, subject, classroom, Class
 
-# Register your models here.
+admin.site.register(teacher)
+admin.site.register(group)
+admin.site.register(subject)
+admin.site.register(classroom)
+admin.site.register(Class)
--- main/main_site_page/models.py
+++ main/main_site_page/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-class Class(models.Model):
-    subject = models.ForeignKey(subject, on_delete=models.PROTECT)
-    teacher = models.ForeignKey(teacher, on_delete=models.PROTECT)
-    date = models.DateField()
-    time = models.DurationField()
-    group = models.ForeignKey(group, on_delete=models.PROTECT)
-
 class teacher(models.Model):
     name = models.CharField(max_length=20)
     subject = models.CharField(max_length=20)
@@ -23,3 +16,10 @@ class classroom(models.Model):
     name_teacher = models.CharField(max_length=20)
     date = models.DateField()
     time = models.DurationField()
+
+class Class(models.Model):
+    subject = models.ForeignKey(subject, on_delete=models.PROTECT)
+    teacher = models.ForeignKey(teacher, on_delete=models.PROTECT)
+    date = models.DateField()
+    time = models.DurationField()
+    group = models.ForeignKey(group, on_delete=models.PROTECT)
